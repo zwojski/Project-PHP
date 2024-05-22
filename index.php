defd0ddde09ff0c09d823ea80a24e644bffc49ea
Pawel <zwojski@gmail.com>
2024-05-22 17:56:00 +0200

    action=create

--- index.php
+++ index.php
@@ -1,8 +1,64 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App;
+
 require_once("src/Utils/debug.php");
 
-$test = 'test';
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = null;
+}
 
-dump($test);
+
+?>
+
+<html>
+
+    <head>
+    </head>
+
+    <body>
+        <div class="heder">
+            <h1>Moje notatki</h1>
+        </div>
+        
+        <div class="container">
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="/">Lista notatek</a>
+                    </li>
+                    <li>
+                        <a href="/?action=create">Nowa notatka</a> <!-- dodanie ? wskazuje na parametr -->
+                    </li>
+                   
+                </ul>
+            </div>
+
+            <div>
+                <?php
+                // if($action === 'create'):
+                //     echo 'nowa notatka';
+                // else :
+                //     echo 'list noatek';
+                // endif;               
+                ?>
+                <?php if ($action === 'create') : ?>
+                   <h3> nowa notatka</h3>
+                <?php else : ?>
+                    <h4>lista notatek</h4>
+                <?php endif; ?>
+            </div>
+
+        </div>
+
+        <div class="footer">
+
+        </div>
+    </body>
+    
+</html>
 
--- src/Utils/debug.php
+++ src/Utils/debug.php
@@ -1,5 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+// instrukcja służąca do włączenia wyświetlania wszystkich błędów, które wystąpią w naszym kodzie
+error_reporting(E_ALL);
+// druga komenda to włączenie wyświetlenia błędów; te dwie komendy zawsze występują, jeśli chcemy mieć pełny zakres zgłaszanych błędów to musimy ich użyć jednocześnie
+ini_set('display_errors', '1');
+
 function dump($data)
 {
   echo '<div 
